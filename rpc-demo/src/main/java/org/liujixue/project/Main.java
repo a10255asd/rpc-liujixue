e60f27b9a3c758be0f154d14a66741788a338365
刘鸡血 <1025519998@qq.com>
2023-08-16 15:16:55 +0800

    add: new project
    add:
    
    new project

---
+++ rpc-demo/src/main/java/org/liujixue/project/Main.java
@@ -0,0 +1,16 @@
+package org.liujixue.project;
+
+/**
+ * @Author LiuJixue
+ * @Date 2023/8/16 13:59
+ * @PackageName:org.liujixue.project
+ * @ClassName: ${NAME}
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ rpc-demo/src/test/java/com/liujixue/NettyTest.java
@@ -0,0 +1,32 @@
+package com.liujixue;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+import org.junit.Test;
+
+/**
+ * @Author: LiuJixue
+ * @Date: 2023/8/16 14:50
+ * @PackageName:com.liujixue
+ * @ClassName: NettyTest
+ * @Description: netty 测试类
+ * @Version 1.0
+ */
+public class NettyTest {
+    @Test
+    public void testByteBuf() {
+        ByteBuf header = Unpooled.buffer();
+        ByteBuf body = Unpooled.buffer();
+        // 通过逻辑组装，而不是物理拷贝，实现在JVM中零拷贝
+        CompositeByteBuf byteBuf = Unpooled.compositeBuffer();
+        byteBuf.addComponents(header,body);
+    }
+    @Test
+    public void testWrapper(){
+        byte[] buf = new byte[1024];
+        byte[] buf2 = new byte[1024];
+        // 共享byte数组的内容而不是拷贝，这也算零拷贝
+        ByteBuf byteBuf = Unpooled.wrappedBuffer(buf, buf2);
+    }
+}
